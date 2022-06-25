dd5c6c546dcbfe55348ae0f5da0a13c1362964ce
Enrico Shippole <enricoship@gmail.com>
2022-06-24 23:36:09 -0400

    Update train_sentencepiece_tokenizer.py

--- train_sentencepiece_tokenizer.py
+++ train_sentencepiece_tokenizer.py
@@ -1,19 +1,30 @@
 from datasets import load_dataset
-from tokenizers import SentencePieceBPETokenizer
+#from tokenizers import SentencePieceBPETokenizer
+import io
+import sentencepiece as spm
 
-dataset = load_dataset('the_pile/all', split='train', streaming=True)
+dataset = load_dataset('conceptofmind/pile_wikipedia_en', split='train', streaming=True)
 
-tokenizer = SentencePieceBPETokenizer()
+# tokenizer = SentencePieceBPETokenizer()
 
 def batch_iterator(dataset):
     for i in dataset:
         yield i["text"]
+        
+model = io.BytesIO()
 
-tokenizer.train_from_iterator(
-    text='',
-    vocab_size=32_000,
-    min_frequency=2,
-    show_progress=True,
+spm.SentencePieceTrainer.train(
+    sentence_iterator = batch_iterator(dataset), 
+    model_writer=model, 
+    vocab_size=32000,
+    model_type='bpe',
 )
 
-tokenizer.save()
+# tokenizer.train_from_iterator(
+#     text='',
+#     vocab_size=32_000,
+#     min_frequency=2,
+#     show_progress=True,
+# )
+
+# tokenizer.save()
