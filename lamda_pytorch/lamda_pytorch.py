f736bd177ccfe1c37f3d138a4578eddbc5dbc67a
Aleksandar Ivanovski <aleksandar.ivanovski123@gmail.com>
2022-07-05 21:51:51 +0200

    Add poetry packaging

--- lamda_pytorch/lamda_pytorch.py
+++ lamda_pytorch/lamda_pytorch.py
@@ -6,9 +6,9 @@ import math
 
 from einops import rearrange
 
-from config.config import CFG
+from .config.config import CFG
 
-from utils.utils import AutoregressiveWrapper
+from .utils.utils import AutoregressiveWrapper
 
 # residual wrapper
 
